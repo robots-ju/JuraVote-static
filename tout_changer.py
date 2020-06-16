32dbef7c0eb412857acb1ef27ec00b424651eb33
XZ1048576 <sylvain.lovis@gmail.com>
2020-06-17 00:05:49 +0200

    ajout du lien actualitÃs dans la navbar mobile

--- tout_changer.py
+++ tout_changer.py
@@ -10,51 +10,28 @@ def all_files(path,ext):
         elif isdir(x):
             r+=all_files(x,ext)
     return r
-def search_balise(ctn,balise,class_):
-    r=[]
-    balise=balise.encode()
-    class_=class_.encode()
-    k=ctn.find(b"<"+balise)
-    while k!=-1:
-        start=k
-        end=ctn.find(b">",start)
-        balise=ctn[start:end]
-        if class_==b"":
-            r.append((start,end))
-            k=ctn.find(b"<"+balise,k+1)
-            continue
-        class_pos=balise.find(b"class=\"")
-        if class_pos!=-1:
-            start=class_pos+7
-            end=balise.find(b'"',start)
-            if class_ in balise[start:end].split():
-                r.append((start,end))
-        k=ctn.find(b"<"+balise,k+1)
-    return r
 for x in all_files(".","html"):
     with open(x,"rb")as f:
         ctn=f.read()     
-#titres en h1
-    k=len(search_balise(ctn,"h1","")),len(search_balise(ctn,"h2",""))
-    if k==(0,0):
-        continue
-    elif k==(0,1):
-        title=search_balise(ctn,"h2","")
-    else:
-        title=search_balise(ctn,"h1","")
-    [(start,end)]=title
-    end=list(ctn[start:start+3])
-    end.insert(1,47)
-    end.append(62)
-    end=bytes(end)
-    end=ctn.find(end,start)+len(end)
-    title=ctn[start:end]
-    title=title[title.find(b">")+1:title.find(b"<",3)]
-    title=b"<h1 class=\"uk-heading-large\">"+title+b"</h1>"
-    title=list(title)
-    ctn=list(ctn)
-    ctn[start:end]=title
-    ctn=bytes(ctn)
-    with open(x,"wb")as f:
-        f.write(ctn)
 #actualit&eacute;s navbar mobile + normale
+    m_start=ctn.find(b"""<div id="menu-mobile" uk-offcanvas>""")
+    if m_start==-1:
+        continue
+    end=ctn.find(b"</div>",ctn.find(b"</div>",m_start)+1)
+    part=ctn[m_start:end]
+    start=part.find(b"""07Instances/accueilI.html">Instances polit""")
+    i=1
+    while part[start-3*i]==46:
+        i+=1
+    i-=1
+    start=part.find(b"</li>",start)
+    start=part.find(b"\n",start)
+    start+=1
+    old=start
+    start=part.find(b"\n",start)
+    indent=b"\n"+part[old:start-1]
+    new=b"""<li id="Actualites">
+####    <a class="uk-button uk-button-text" href=\""""+b"../"*i+b"""09Actualites/Actualites.html">Actualit&eaccute;s politiques</a>
+####</li>"""
+    with open(x,"wb")as f:
+        f.write(ctn[:m_start+start-1]+new.replace(b"####",indent)+ctn[m_start+start-1:])

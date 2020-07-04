de379aaa0249eda94830b32d457ed0cc70c786ae
XZ1048576 <sylvain.lovis@gmail.com>
2020-07-04 14:28:52 +0200

    envoyer les donnees de nos utilisateurs a google

--- tout_changer.py
+++ tout_changer.py
@@ -13,25 +13,22 @@ def all_files(path,ext):
 for x in all_files(".","html"):
     with open(x,"rb")as f:
         ctn=f.read()     
-#actualit&eacute;s navbar mobile + normale
-    m_start=ctn.find(b"""<div id="menu-mobile" uk-offcanvas>""")
-    if m_start==-1:
+    ##########################################
+    #### modifications de chaque page ici ####
+    ##########################################
+    if ctn.count(b"dataLayer")!=0:
         continue
-    end=ctn.find(b"</div>",ctn.find(b"</div>",m_start)+1)
-    part=ctn[m_start:end]
-    start=part.find(b"""07Instances/accueilI.html">Instances polit""")
-    i=1
-    while part[start-3*i]==46:
-        i+=1
-    i-=1
-    start=part.find(b"</li>",start)
-    start=part.find(b"\n",start)
-    start+=1
-    old=start
-    start=part.find(b"\n",start)
-    indent=b"\n"+part[old:start-1]
-    new=b"""<li id="Actualites">
-####    <a class="uk-button uk-button-text" href=\""""+b"../"*i+b"""09Actualites/Actualites.html">Actualit&eaccute;s politiques</a>
-####</li>"""
+    ctn=ctn.replace(b"</head>",b"""    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-166072477-1');
+    </script>
+</head>""")
     with open(x,"wb")as f:
-        f.write(ctn[:m_start+start-1]+new.replace(b"####",indent)+ctn[m_start+start-1:])
+        f.write(ctn)
